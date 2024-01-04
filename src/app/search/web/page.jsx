a010c176fe4832b5680b623659db5ab84d4e2bb9
Shahriyor <shahriyoradhamov@icloud.com>
2024-01-04 21:24:36 +0500

    добавил апи для запроса

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function Web() {
+async function Web({searchParams}) {
+
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CX}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items
   return (
-    <div>Web</div>
+    <>
+      {
+        results && results.map(result => (
+          <h1 key={result.cacheId}>{result.title}</h1>
+        ))
+      }    
+    </>
   )
 }
 
